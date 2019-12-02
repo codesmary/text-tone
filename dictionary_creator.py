ebd3adc3b53b1a4a0f809e06de880c865845f7a1
Rosemary Fortanely <rosemary.fortanely@gmail.com>
2019-12-02 16:27:51 -0600

    update dictionary creation and model architecture

--- dictionary_creator.py
+++ dictionary_creator.py
@@ -1,15 +1,15 @@
-from keras.preprocessing.text import Tokenizer, text_to_word_sequence
+from keras.preprocessing.text import Tokenizer
 import numpy as np
 import sqlite3
-import string
 import csv
-import re
 import os
+import string
+import re
 
 MAX_SEQUENCE_LENGTH = 25
-MAX_NB_WORDS = 20000
+MAX_NB_WORDS = 5000
 
-#create vocab from first 1,000,000 reddit comments
+#create vocab from first 2,000,000 reddit comments
 conn = sqlite3.connect("data/reddit-comments.sqlite")
 c = conn.cursor()
 
@@ -17,12 +17,15 @@ texts = []
 ct = 0
 for row in c.execute('SELECT body FROM May2015'):
     row = row[0].split()
+    '''
     text = ''
     for word in row:
         if len(text) + len(word) > MAX_SEQUENCE_LENGTH:
             break
         text += word
     texts.append(text)
+    '''
+    texts.append(row)
     ct += 1
     if ct == 1000000:
         break
@@ -34,10 +37,24 @@ for word in f:
     texts.append(word)
 
 #create Tokenizer
-tokenizer = Tokenizer(MAX_NB_WORDS + 1, oov_token='unk')
+tokenizer = Tokenizer(MAX_NB_WORDS, oov_token='unk')
 tokenizer.fit_on_texts(texts)
 tokenizer.word_index = {e:i for e,i in tokenizer.word_index.items() if i <= MAX_NB_WORDS}
-tokenizer.word_index[tokenizer.oov_token] = MAX_NB_WORDS + 1
+tokenizer.word_index[tokenizer.oov_token] = 0
 
 def get_mapping(words: str):
-    return tokenizer.texts_to_sequences(words)
+    mappings = []
+    words = words.lower()
+    words = re.sub('[^%s]' % (string.ascii_lowercase + '\'\- '), ' ', words)
+    for word in words.split(' '):
+        if not word:
+            continue
+
+        try:
+            mapping = tokenizer.word_index[word]
+        except:
+            mapping = tokenizer.word_index[tokenizer.oov_token]
+
+        mappings.append(mapping)
+
+    return mappings
--- sentiment_csv_generator.py
+++ sentiment_csv_generator.py
@@ -3,53 +3,79 @@ import dictionary_creator
 import numpy as np
 from keras.layers.embeddings import Embedding
 from keras.models import Model, Sequential
-from keras.layers import Flatten, Dense
+from keras.layers.convolutional import Conv1D, MaxPooling1D
+from keras.layers import Flatten, Dense, Dropout, BatchNormalization, Activation
 from keras.preprocessing import sequence
 from keras.optimizers import Adam
 import sqlite3
-import string
 import csv
-import re
 import os
 
-#create indexed sets for our reddit data
-original_phrases = []
-x = []
-conn = sqlite3.connect("data/reddit-comments.sqlite")
-c = conn.cursor()
-ct = 0
-
-for row in c.execute('SELECT body FROM May2015'):
-    row = row[0]
-    sentence_embedding = dictionary_creator.get_mapping(row)
-    original_phrases.append(row)
-    x.append(sentence_embedding)
-    ct += 1
-    print(ct)
-    if ct == 1000000:
-        break
-
-x = np.array(x)
-x = sequence.pad_sequences(x, maxlen=500)
+chunk_size = 1000000
 
 #load the sentiment model back in
-model = Sequential()
-model.add(Embedding(5000, 32, input_length=500))
-model.add(Flatten())
-model.add(Dense(250, activation = "relu"))
-model.add(Dense(50, activation = "relu"))
-model.add(Dense(1, activation = "sigmoid"))
+def load_model():
+    model = Sequential()
+    model.add(Embedding(5001, 32, input_length=500))
+    model.add(BatchNormalization())
+    model.add(Conv1D(filters=32, kernel_size=7, padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling1D())
+    model.add(Conv1D(filters=64, kernel_size=3, padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling1D())
+    model.add(Flatten())
+    model.add(Dense(250, activation='relu'))
+    model.add(Dropout(rate=0.5))
+    model.add(Dense(1, activation='sigmoid'))
 
-model.load_weights('models/sent_nn.h5')
+    model.load_weights(os.path.join('models', 'sent_nn.h5'))
+    print("Model weights loaded.")
+
+    return model
+
+#create indexed sets for our reddit data
+def prepare_data(chunk_offset: int):
+    print("Preparing data.")
+    original_phrases = []
+    x = []
+    conn = sqlite3.connect(os.path.join("data","reddit-comments.sqlite"))
+    c = conn.cursor()
+
+    for row in c.execute('SELECT body FROM May2015 LIMIT ? OFFSET ?',\
+                        (chunk_size, chunk_offset*chunk_size)):
+        row = row[0]
+        sentence_embedding = dictionary_creator.get_mapping(row)
+        original_phrases.append(row)
+        x.append(sentence_embedding)
+
+    x = np.array(x)
+    x = sequence.pad_sequences(x, maxlen=500)
+
+    print("Original phrases and x aggregated.")
+    return original_phrases, x
 
 #create the csv file with the predicted sentiments
-with open(os.path.join('data','reddit-comments-sentiment.csv'), 'w') as csvfile:
-    filewriter = csv.writer(csvfile)
-    filewriter.writerow(['Sentiment', 'Text'])
-    for i in range(0, 1000000 - 32, 32):
-        print(i)
-        prediction = model.predict(x[i:32+i])
-        for j in range(32):
-            sentiment = 1 if prediction[j] >= 0.5 else 0
-            text = original_phrases[i+j].replace('\n', ' ')
-            filewriter.writerow([sentiment, text])
+def write_to_file(initial_write: bool, model, original_phrases, x):
+    print("Beginning writing to file.")
+    with open(os.path.join('data','reddit-comments-sentiment.csv'),\
+                           'w' if initial_write else 'a') as csvfile:
+        filewriter = csv.writer(csvfile)
+        if initial_write:
+            filewriter.writerow(['Sentiment', 'Text'])
+        for i in range(0, chunk_size - 32, 32):
+            prediction = model.predict(x[i:32+i])
+            for j in range(32):
+                sentiment = 1 if prediction[j] >= 0.5 else 0
+                text = original_phrases[i+j].replace('\n', ' ')
+                filewriter.writerow([sentiment, text])
+    print("File " + ("written to." if initial_write else "appended to."))
+
+model = load_model()
+been_first = True
+for i in range(5):
+    original_phrases, x = prepare_data(i)
+    write_to_file(been_first, model, original_phrases, x)
+    been_first = False
